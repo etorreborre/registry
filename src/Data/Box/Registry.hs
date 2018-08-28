9b9887ccaf0b33a7a3bb22792cfe80b8d2091e53
Eric Torreborre <etorreborre@yahoo.com>
2018-08-28 11:00:32 +0200

    removed the monoid instance for Registry

--- src/Data/Box/Registry.hs
+++ src/Data/Box/Registry.hs
@@ -97,8 +97,8 @@ instance Semigroup (Registry inputs outputs) => Monoid (Registry inputs outputs)
   mempty = Registry (Values []) (Functions []) (Specializations []) (Modifiers [])
   mappend = (<>)
 
-instance Semigroup (Registry inputs outputs) where
-  (<>) (Registry (Values vs1) (Functions fs1) (Specializations ss1) (Modifiers ms1))
+(<+>) :: Registry is1 os1 -> Registry is2 os2 -> Registry (is1 :++ is2) (os1 :++ os2)
+(<+>) (Registry (Values vs1) (Functions fs1) (Specializations ss1) (Modifiers ms1))
        (Registry (Values vs2) (Functions fs2) (Specializations ss2) (Modifiers ms2))  =
           Registry (Values (vs1 <> vs2)) (Functions (fs1 <> fs2)) (Specializations (ss1 <> ss2)) (Modifiers (ms1 <> ms2))
 
