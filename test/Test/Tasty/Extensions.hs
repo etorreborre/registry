996a99a41dc925e5db235aa0cf562b6609c62e8b
Eric Torreborre <etorreborre@yahoo.com>
2018-09-20 16:01:44 +0200

    added noShrink and withSeed options for testing

--- test/Test/Tasty/Extensions.hs
+++ test/Test/Tasty/Extensions.hs
@@ -17,17 +17,22 @@ module Test.Tasty.Extensions (
   module Hedgehog
 , module Tasty
 , gotException
+, noShrink
 , prop
 , test
 , minTestsOk
+, withSeed
 ) where
 
+import           Data.Maybe          (fromJust)
 import           GHC.Stack
 import           Hedgehog            as Hedgehog hiding (test)
 import           Hedgehog.Corpus     as Hedgehog
 import           Hedgehog.Gen        as Hedgehog hiding (discard, print)
+import qualified Prelude             as Prelude
 import           Protolude           hiding ((.&.))
 import           Test.Tasty          as Tasty
+import           Test.Tasty.Options  as Tasty
 import           Test.Tasty.Hedgehog as Tasty
 import           Test.Tasty.TH       as Tasty
 
@@ -37,8 +42,7 @@ prop name p = [withFrozenCallStack $ testProperty name (Hedgehog.property p)]
 
 -- | Create a Tasty test from a Hedgehog property called only once
 test :: HasCallStack => TestName -> PropertyT IO () -> [TestTree]
-test name p = withFrozenCallStack $
-  minTestsOk 1  . localOption (HedgehogShrinkLimit (Just (0 :: ShrinkLimit))) <$> prop name p
+test name p = withFrozenCallStack (minTestsOk 1 . noShrink $ prop name p)
 
 gotException :: forall a . (HasCallStack, Show a) => a -> PropertyT IO ()
 gotException a = withFrozenCallStack $ do
@@ -47,9 +51,13 @@ gotException a = withFrozenCallStack $ do
     Left _  -> assert True
     Right _ -> annotateShow ("excepted an exception" :: Text) >> assert False
 
-
-
 -- * Parameters
 
-minTestsOk :: Int -> (TestTree -> TestTree)
-minTestsOk n = localOption (HedgehogTestLimit (Just (fromInteger (toInteger n))))
+minTestsOk :: Int -> [TestTree] -> [TestTree]
+minTestsOk n = fmap (localOption (HedgehogTestLimit (Just (toEnum n :: TestLimit))))
+
+noShrink :: [TestTree] -> [TestTree]
+noShrink = fmap (localOption (HedgehogShrinkLimit (Just (0 :: ShrinkLimit))))
+
+withSeed :: Prelude.String -> [TestTree] -> [TestTree]
+withSeed seed = fmap (localOption (fromJust (parseValue seed :: Maybe HedgehogReplay)))
