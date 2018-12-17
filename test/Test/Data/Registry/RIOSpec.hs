b55f673088cdbf4a8b471ec9027a75a3307d7c57
Eric Torreborre <etorreborre@yahoo.com>
2018-12-17 09:34:41 +0100

    use tasty < 1.3

--- test/Test/Data/Registry/RIOSpec.hs
+++ test/Test/Data/Registry/RIOSpec.hs
@@ -36,22 +36,4 @@ test_close_resource_no_warmup = prop "RIO resources must be closed" $ do
   content <- liftIO $ readIORef ref
   content === ["start", "use", "close" :: Text]
 
--- withNoWarmupRIO :: RIO a -> (a -> IO b) -> IO b
-
--- withRIO :: RIO a -> (a -> IO ()) -> IO Result
-
--- withRegistry :: forall a b ins out . (Typeable a, Contains (RIO a) out, Solvable ins out) =>
---      Registry ins out
---   -> (Result -> a -> IO b)
---   -> IO b
-
--- runRegistryT :: forall a ins out . (Typeable a, Contains (RIO a) out, Solvable ins out) => Registry ins out -> ResourceT IO (a, Warmup)
--- executeRegistry :: forall a ins out . (Typeable a, Contains (RIO a) out, Solvable ins out) => Registry ins out -> IO (a, Warmup, Stop)
--- unsafeRun :: forall a ins out . (Typeable a, Contains (RIO a) out) => Registry ins out -> IO a
--- unsafeRunDynamic :: forall a ins out . (Typeable a) => Registry ins out -> IO a
--- unsafeRunWithStop :: forall a ins out . (Typeable a, Contains (RIO a) out) => Registry ins out -> IO (a, Stop)
--- unsafeRunDynamicWithStop :: forall a ins out . (Typeable a) => Registry ins out -> IO (a, Stop)
--- warmupWith :: Warmup -> RIO ()
--- allocate :: IO a -> (a -> IO ()) -> RIO a
-
 tests = $(testGroupGenerator)
